70a885f6c2bd843c2a8c525fe9cc4f15c5b3f0c1
NyPadilha <763npp@gmail.com>
2023-11-23 16:30:21 -0300

    <test>[ counter ]: <count occurrences test>

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,14 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+# Quase certeza que é occurrences e nao ocurrences
 
 
 def test_counter():
-    raise
+    path = "data/jobs.csv"
+
+    assert count_ocurrences(path, "python") == 1639
+
+    assert count_ocurrences(path, "Jaguara") == 0
+
+    assert count_ocurrences(path, "pYtHOn") == 1639
+
+    assert count_ocurrences(path, "data engineer") == 1528
